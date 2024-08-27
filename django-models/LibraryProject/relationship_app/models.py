fa7e5f931e0a3f8d254134a8bf2e8d2ab816eabc
pkathuri <pkathuri19@gmail.com>
2024-08-27 16:53:07 +0300

    final commit

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
@@ -32,27 +30,3 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=['Admin', 'Librarian', 'Member'])
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_date = models.DateField()
-
-    class Meta:
-        permissions = (
-            ('is_admin', 'Is Admin'),
-            ('can_add_book', 'Can add book'),
-            ('can_change_book', 'Can change book'),
-            ('can_delete_book', 'Can delete book'),
-        )
-    

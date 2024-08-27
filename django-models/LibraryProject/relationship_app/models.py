b5baad01d56eba3b64867069406a48f19c94fcf7
pkathuri <pkathuri19@gmail.com>
2024-08-27 16:48:28 +0300

    my commit

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,27 +3,37 @@ from django.db import models
 # from users.models import User
 # Create your models here.
 # The Author model
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 class Author(models.Model):
-    name = models.CharField(max_length = 255)
+    name = models.CharField(max_length=255)
+
     def __str__(self):
         return self.name
 
+
+
 class Book(models.Model):
-    title = models.CharField(max_length = 255)
-    author = models.ForeignKey(Author,on_delete = models.CASCADE)
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
 
 class Library(models.Model):
-    name = models.CharField(max_length = 255)
+    name = models.CharField(max_length=255)
     books = models.ManyToManyField(Book)
+
     def __str__(self):
         return self.name
 
 class Librarian(models.Model):
-    name = models.CharField(max_length = 255)
-    library = models.OneToOneField(Library, on_delete = models.CASCADE)
+    name = models.CharField(max_length=255)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
-    
     

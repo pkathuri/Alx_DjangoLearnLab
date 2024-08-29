ea5b7addf8b53d66950c843ec2f6a338012d5798
pkathuri <pkathuri19@gmail.com>
2024-08-29 10:13:31 +0300

    books by a specific author commited

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,9 +1,6 @@
 from relationship_app.models import Book
 from relationship_app.models import Library
 
-# Obtaining all books by a specific author
-books = Book.objects.get(author="John Doe")
-# obtaining all books in a library
-library_books = Library.books.all()
-# Obtaining all the
+# A query of all books by a specific author
+books_by_author = Book.objects.filter(author = "John Doe")
 

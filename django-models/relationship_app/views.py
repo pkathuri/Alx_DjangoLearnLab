0ade27bfdcf76c5a9d3dfab6e6ac2ccd5968c012
pkathuri <pkathuri19@gmail.com>
2024-08-29 12:11:54 +0300

    commit

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from .models import Book
+from .models import Library
 # Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'templates/relationship_app/list_books.html', {'books': books})

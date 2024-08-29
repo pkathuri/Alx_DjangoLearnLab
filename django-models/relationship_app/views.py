60f041b23f97681079d3fb2b19e98d08e21c5399
pkathuri <pkathuri19@gmail.com>
2024-08-29 17:43:20 +0300

    library view created

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,6 +4,11 @@ from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 # Create your views here.
+# Finctional view for the book model
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'templates/relationship_app/list_books.html', {'books': books})
+# Class view for the library model
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'templates/relationship_app/library_detail.html'

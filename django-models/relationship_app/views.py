135483cd6727a4b6e9ee9e1e23a0e8cc8b080827
pkathuri <pkathuri19@gmail.com>
2024-08-31 11:03:07 +0300

    views complete

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.views import LoginView,LogoutView
 from .models import Book
 from .models import Library
 # Create your views here.
@@ -12,3 +14,22 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'templates/relationship_app/library_detail.html'
+# The registration registration view
+# This is a function based view
+def register(request):
+    if request.method == "POST":
+        # A form for creating new users providing a username and password.
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+        else:
+            form = UserCreationForm()
+        return render(request,"relationship_app/templates/register.html",{"form":form})
+# The login view
+class LoginView(LoginView):
+    template_name = "login.html"
+
+# The logout view
+class LogoutView(LogoutView):
+    template_name = "logout.html"
